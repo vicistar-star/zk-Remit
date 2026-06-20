2978968bd2d9023176b67d00b33280280a3e433e
vicistar-star <ogazivictor17@gmail.com>
2026-06-20 14:18:21 +0100

    feat(backend): add credential issuance and issuers endpoints

--- backend/src/credential/credential.controller.ts
+++ backend/src/credential/credential.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Body, ValidationPipe, UsePipes } from '@nestjs/common';
+import { CredentialService } from './credential.service';
+import { IssueCredentialDto } from './dto/issue-credential.dto';
 
 @Controller('credential')
-export class CredentialController {}
+export class CredentialController {
+  constructor(private readonly credentialService: CredentialService) {}
+
+  @Post('issue')
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  async issue(@Body() dto: IssueCredentialDto) {
+    return this.credentialService.issue(dto);
+  }
+
+  @Get('issuers')
+  async getIssuers() {
+    return this.credentialService.getIssuers();
+  }
+}
