142211666f5ab3feeb051bc507a41150617e007a
vicistar-star <ogazivictor17@gmail.com>
2026-06-22 18:40:59 +0100

    feat: configure Angular build with esbuild and tailwind

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000',
+  stellarNetwork: 'testnet' as const,
+  stellarRpcUrl: 'https://soroban-testnet.stellar.org',
+  stellarHorizonUrl: 'https://horizon-testnet.stellar.org',
+  verifierContractId: '',
+};
